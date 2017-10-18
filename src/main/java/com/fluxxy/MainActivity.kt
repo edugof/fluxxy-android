85fe10a74d523c0d671ab01a91d468a02bdf0896
edugof <edugof@gmail.com>
2017-10-18 20:39:07 +0200

    Add first call to AWS, just test service.

--- src/main/java/com/fluxxy/MainActivity.kt
+++ src/main/java/com/fluxxy/MainActivity.kt
@@ -4,15 +4,39 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.StringRequestListener
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import okhttp3.OkHttpClient
+
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val okHttpClient = OkHttpClient().newBuilder()
+                .addNetworkInterceptor(StethoInterceptor())
+                .build()
+        AndroidNetworking.initialize(applicationContext, okHttpClient)
         setContentView(R.layout.activity_main)
     }
 
     fun sendMessage(view: View) {
-        Log.i(Constants.LOG, "Button was pressed")
+        Log.i(Constants.LOG, "Button clicked!")
+        AndroidNetworking.get("http://18.221.145.123:8000/")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsString(object : StringRequestListener {
+                    override fun onResponse(response: String) {
+                        Log.i(Constants.LOG, "Response: $response")
+                    }
+
+                    override fun onError(error: ANError) {
+                        Log.i(Constants.LOG, "Error: ${error.errorBody}")
+                    }
+                })
     }
 }
